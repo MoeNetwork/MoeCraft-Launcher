683c5e2ad56ffd52928ac84e6da4635080936e9f
Kenvix <kenvix@vip.qq.com>
2016-06-11 11:48:16 +0800

    2.0

--- MCLauncher/Program.cs
+++ MCLauncher/Program.cs
@@ -19,37 +19,82 @@ namespace MCLauncher
             Application.EnableVisualStyles();
             if (!File.Exists(launcher))
             {
-                error("找不到启动器文件：" + launcher + "\r\n请打开更新器更新 MoeCraft");
+                if(File.Exists(path + "启动器.jar"))
+                {
+                    launcher = path + "启动器.jar";
+                }
+                else
+                {
+                    launcher = "";
+                    var df = Directory.GetFiles(path);
+                    foreach(string la in df)
+                    {
+                        var ff = new FileInfo(la);
+                        if(ff.Name.Substring(0,4).ToLower() == "hmcl")
+                        {
+                            launcher = la;
+                            break;
+                        }
+                    }
+                    if(string.IsNullOrEmpty(launcher))
+                    {
+                        error("找不到启动器文件\r\nLauncher.jar 或 启动器.jar 或 HMCL*.jar\r\n\r\n请打开更新器更新 MoeCraft");
+                    }
+                }
             }
             else
             {
                 try
                 {
-                    RegistryKey regRoot = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\JavaSoft\Java Runtime Environment");
-                    RegistryKey reg     = regRoot.OpenSubKey((regRoot.GetSubKeyNames())[regRoot.GetSubKeyNames().Length - 1]);
-                    string javahome     = reg.GetValue("JavaHome").ToString();
-                    string path         = javahome + @"\bin\javaw.exe";
-                    if (javahome == null || !File.Exists(path))
-                    {
-                        path = "javaw.exe";
-                    }
-                    Process.Start(path, "-jar \"" + launcher + "\" ");
+                    java("java.exe");
                 }
-                catch(Exception ex)
+                catch (Exception)
                 {
-                    if (MessageBox.Show("你的电脑可能没有安装 Java，你可以从以下地址获取 Java：\r\nhttps://cdn.moecraft.net/jre/index.html\r\n你必须安装 Java 才能运行 MoeCraft，现在打开该地址吗？\r\n\r\n" + ex.Message, "Java 错误 - MoeCraft Launcher for Windows", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
+                    try
                     {
-                        try
+                        RegistryKey regRoot = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\JavaSoft\Java Runtime Environment");
+                        RegistryKey reg     = regRoot.OpenSubKey((regRoot.GetSubKeyNames())[regRoot.GetSubKeyNames().Length - 1]);
+                        string javahome     = reg.GetValue("JavaHome").ToString();
+                        string path         = javahome + @"\bin\java.exe";
+                        if (string.IsNullOrEmpty(javahome) || !File.Exists(path))
                         {
-                            Process.Start("https://cdn.moecraft.net/jre/index.html");
+                            if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("JAVA_HOME")))
+                            {
+                                path         = Environment.GetEnvironmentVariable("JAVA_HOME") + @"\bin\java.exe";
+                                if (!File.Exists(path))
+                                {
+                                    path = "java.exe";
+                                }
+                            }
                         }
-                        catch (Exception pex)
+                        java(path);
+                    }
+                    catch (Exception ex)
+                    {
+                        if (MessageBox.Show("你的电脑可能没有安装 Java，你可以从以下地址获取 Java：\r\nhttps://cdn.moecraft.net/jre/index.html\r\n你必须安装 Java 才能运行 MoeCraft，现在打开该地址吗？\r\n\r\n" + ex.Message, "Java 错误 - MoeCraft Launcher for Windows", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
                         {
-                            error(pex.Message);
+                            try
+                            {
+                                Process.Start("https://cdn.moecraft.net/jre/index.html");
+                            }
+                            catch (Exception pex)
+                            {
+                                error(pex.Message);
+                            }
                         }
                     }
                 }
             }
+        }
+
+        private static void java(string v)
+        {
+            var ps = new Process();
+            ps.StartInfo.UseShellExecute = false;
+            ps.StartInfo.CreateNoWindow = true;
+            ps.StartInfo.Arguments = "-jar \"" + launcher + "\" ";
+            ps.StartInfo.FileName = v;
+            Application.Run(new logger(ps));
         }
 
         public static void error(string msg, string title = "错误 - MoeCraft Launcher for Windows")
---
+++ MCLauncher/log.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MCLauncher
+{
+    public partial class logger : Form
+    {
+        public Process px;
+
+        public logger(Process ps)
+        {
+            InitializeComponent();
+            ps.StartInfo.RedirectStandardOutput = true;
+            ps.Exited += Ps_Exited;
+            ps.Start();
+            ps.BeginOutputReadLine();
+            px = ps;
+            ps.OutputDataReceived += new DataReceivedEventHandler((object drsender, DataReceivedEventArgs dre) =>
+            {
+                log(dre.Data);
+            });
+        }
+
+        private void Ps_Exited(object sender, EventArgs e)
+        {
+            Text += " - 游戏已退出";
+        }
+
+        public void error(string msg, string title = "错误")
+        {
+            MessageBox.Show(msg, title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void 清空LToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            text.Text = "";
+        }
+
+        private void 全选AToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            text.SelectAll();
+        }
+
+        private void 复制CToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public delegate void logInvoke(string v);
+        public void log(string v)
+        {
+            if (text.InvokeRequired)
+            {
+                logInvoke li = new logInvoke(log);
+                Invoke(li, v);
+            }
+            else
+            {
+                text.AppendText(v + "\r\n");
+            }
+        }
+
+        private void text_TextChanged(object sender, EventArgs e)
+        {
+            text.ScrollToCaret();
+        }
+
+        private void logger_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                px.Kill();
+            }
+            catch (Exception) { }
+        }
+
+        private void 停止SToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                px.Kill();
+            }
+            catch (Exception) { }
+        }
+
+        private void 关于AToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("MoeCraft Launcher for Windows V2.0\r\n作者：Kenvix\r\nhttp://zhizhe8.net","关于",MessageBoxButtons.OKCancel,MessageBoxIcon.Information) == DialogResult.OK)
+            {
+                try
+                {
+                    Process.Start("http://zhizhe8.net");
+                }
+                catch (Exception) { }
+            }
+        }
+    }
+}
